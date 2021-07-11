9532ee43bfd7bafe6e8acd2a554142945724ba26
SomiaWhitering <q57235562@outlook.com>
2021-07-11 21:55:53 +0800

    update

---
+++ 6-1 字符出现次数统计.py
@@ -0,0 +1,17 @@
+text = input("请输入需要检测的文本")
+text = text.lower()
+sheet = dict()
+logs = list(range(ord('a'),ord('z')))
+for i in logs:
+    sheet.update({chr(i):text.count(chr(i))})
+while sheet!={}:
+    b=c=d=0
+    for i in logs:
+        b = sheet.get(chr(i),0)
+        if b>=d:
+            c,d=i,b
+    else:
+        sheet.pop(chr(c))
+        if d != 0:
+            print("%s出现了%d次" %(chr(c),d))
+        logs.remove(c)
